e84b2167659d4d1539ba1839bd4cf1471fbb94e1
KenKenMkIISR <tanaka-kenji@ll.em-net.ne.jp>
2023-07-01 10:26:25 +0900

    initial commit

---
+++ LCDdriver.h
@@ -0,0 +1,32 @@
+#define VERTICAL 0
+#define HORIZONTAL 1
+#define LCD_ALIGNMENT VERTICAL
+//#define LCD_ALIGNMENT HORIZONTAL
+
+#if LCD_ALIGNMENT == VERTICAL
+	#define X_RES 240 // 横方向解像度
+	#define Y_RES 320 // 縦方向解像度
+#else
+	#define X_RES 320 // 横方向解像度
+	#define Y_RES 240 // 縦方向解像度
+#endif
+
+// LCD settings
+#define LCD_CS 13
+#define LCD_DC 10
+#define LCD_RESET 11
+#define LCD_SPI_TX 15
+#define LCD_SPI_RX 12
+#define LCD_SPI_SCK 14
+#define LCD_SPICH spi1
+
+void LCD_WriteComm(unsigned char comm);
+void LCD_WriteData(unsigned char data);
+void LCD_WriteData2(unsigned short data);
+void LCD_WriteDataN(unsigned char *b,int n);
+void LCD_Init(void);
+void LCD_SetCursor(unsigned short x, unsigned short y);
+void LCD_Clear(unsigned short color);
+void drawPixel(unsigned short x, unsigned short y, unsigned short color);
+
+extern int lcd180turn;
